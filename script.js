b25a60eb274e94c2b832c51dc653727e32a97dbd
NLR32 <89869608+NLR32@users.noreply.github.com>
2025-12-10 19:36:07 +0200

    customers

--- script.js
+++ script.js
@@ -4,19 +4,37 @@
 // Front page loading
 window.onload = () => {
     const h1 = document.querySelector("h1");
-    const typing = document.querySelector(".typing-demo");
-    const navbar = document.querySelector("#navbar");
-    const cta = document.querySelector(".CTA");
+    const typing = document.getElementById("front-type");
+    const navbar = document.getElementById("navbar");
+    const btn = document.getElementById("front-btn");
+
+    console.log(typing, navbar, btn);
 
 
     setTimeout(() => {
         typing.classList.add("start");
 
         navbar.classList.add("show");
-        cta.classList.add("show");
+        btn.classList.add("show");
 
     }, 1500); 
 };
+
+//brands text loading
+const customerText = document.getElementById("customers-text");
+startAnimationOnSeen(customerText, 500, "show");
+
+//start typing
+const customersTyping = document.getElementById("customers-typing");
+startAnimationOnSeen(customersTyping, 500, "start");
+
+const customersBtn = document.getElementById("customers-btn");
+startAnimationOnSeen(customersBtn, 3000, "show");
+
+
+
+
+
 
 // scroll frame
 const frame = document.getElementById('frame');
@@ -44,3 +62,26 @@ window.addEventListener('scroll', function() {
     }
 
 });
+
+
+// FUNCTIONS:
+function startAnimationOnSeen(el, timeout, action){
+    const observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+
+
+            setTimeout(() => {
+            el.classList.add(action);
+        }, timeout); 
+        
+        } else {
+
+
+            el.classList.remove(action);
+        }
+    }, {
+        threshold: 1 
+    });
+
+    observer.observe(el);
+}
